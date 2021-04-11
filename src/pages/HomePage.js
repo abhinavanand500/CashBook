71ba1937f4c18b8910546ed84b3926cfa31ce25c
Abhinav Anand <abhinav.anand500@gmail.com>
2021-04-11 10:23:34 +0530

    Initial Commit and Navigation and all File Structure Set up

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+const Home = () => {
+    return (
+        <View>
+            <Text>Hii</Text>
+        </View>
+    );
+};
+
+export default Home;
